54d568f91d710d3a15d50275937c402e6bef7e07
Guillaume <guillaume.debeaudoin@gmail.com>
2021-11-19 16:01:27 +0100

    etoile posée reste API

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 let url = "asset/movies.json"
 
+
+//  function getInfo(){
+//  let info = document.getElementById("mySearch").value ;
+
+//  allMovies.innerHTML = ""
+
+//  fetch(`https://imdb-internet-movie-database-unofficial.p.rapidapi.com/search/${info}`, {
+//  	"method": "GET",
+//  	"headers": {
+//  		"x-rapidapi-host": "imdb-internet-movie-database-unofficial.p.rapidapi.com",
+//  		"x-rapidapi-key": "f96acaf05cmsh57182a86a1c91cdp1b03c1jsn635ad3578739"
+//  	}
+//  })
+//  .then(response => response.json()) 
+//  .then (data => {
+//     console.log(data.titles[0].title);
+//  console.log(data);
+//  	data.titles.forEach(element => {
+//  })}
+
+
 fetch(url)
     .then(response => response.json())
     .then(data => {
@@ -10,26 +31,37 @@ fetch(url)
             console.log(element.id)
             console.log(element.poster_path)
 
+            let note = '';
+            let vartemp = Math.floor(element.vote_average / 2);
+            for (i = 0; i <= vartemp; i++) {
+                note += `<i class="fas fa-star"></i>`
+            }
+
+            if (vartemp < 5) {
+                for (i = 0; i < 4 - vartemp; i++) {
+                    note += `<i class="far fa-star"></i>`
+                }
+            }
+
+
             let film = element
-            let myAffichage = `<div class="card col-lg-3 m-0" style="max-width: 350px;">
-            <div class="colorCards row">
+            let myAffichage = `<div class="card mt-3 position-relative col-lg-3" style="max-width: 350px;">
+            <div class="colorCards row g-0">
                 <div class="col-lg-4">
                     <img src="${element.poster_path}"
                         class="img-fluid rounded-start" alt="...">
                 </div>
                 <div class="col-lg-8">
                     <div class="card-body">
-                        <h5 class="card-title">${element.original_title}</h5>
+                        <h5 class="card-title text-truncate">${element.original_title}</h5>
                         <p class="card-text para">${element.overview}</p>
-                        <p class="card-text"><small class="text-muted">${element.vote_average}</small></p>
+                        <p class="card-text position-absolute bottom-0 right-0"><small class="text-muted">${element.vote_average} ${note}</small></p>
                     </div>
                 </div>
             </div>
         </div>`;
-        allMovies.innerHTML += myAffichage;
+            allMovies.innerHTML += myAffichage;
 
         });
-
-
 
     })
